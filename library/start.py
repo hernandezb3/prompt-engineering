b269fdeb41e7feb1e9cf71ca2f18bd96f0a4ef51
hernandezb3 <brittney.hernandez@uconn.edu>
2025-04-24 08:40:59 -0400

    adding file paths and additional prompts for generated knowledge

--- library/start.py
+++ library/start.py
@@ -2,6 +2,6 @@ import os
 
 MAIN_DIR = os.getcwd() + "/"
 
-DATA_DIR = "~/Library/CloudStorage/OneDrive-UniversityofConnecticut/project crisp/Anglin, Kylie's files - crisp/data/"
+DATA_DIR = "~/Library/CloudStorage/OneDrive-UniversityofConnecticut/project crisp/Anglin, Kylie's files - crisp/data/clean/"
 
 OUTPUT_DIR = "~/Library/CloudStorage/OneDrive-UniversityofConnecticut/project crisp/Anglin, Kylie's files - crisp/results/"
